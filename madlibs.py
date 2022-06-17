d2359f8c40c684309809d690370f5703c7c39afc
[mikolaj] <[mikolaj.wituszynski@gmail.com]>
2022-06-17 13:50:36 +0200

    madlibs project in python

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+noun1 = input("Noun: ")
+time1 = int(input("Time: "))
+adj1 = input("Adjecive: ")
+noun2 = input("Noun2: ")
+madlib = f"When i woke up i heard strange noise that was coming from the {noun1}. I looked at my smartphone " \
+         f"and saw that it was only {time1} AM. Noise occured again and slowly opened the door and decided to go " \
+         f"downstairs. While i was walking through the corridor suddenly something touched my leg but immedietly" \
+         f" i realised it was just my cat. I grasped and thought too myself i am too {adj1}. I am going back to bed it " \
+         f"was just my cat but when i turned back I felt a fierce breath on the back of my neck and then i knew who " \
+         f"visted me it was {noun2}. "
+print(madlib)
